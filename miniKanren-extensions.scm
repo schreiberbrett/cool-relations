7418c4ade678220a9e5d628d073fa8a580b61eed
Brett Schreiber <schreiberbrett@gmail.com>
2022-05-06 23:47:11 -0400

    Added NP-complete problems.

--- miniKanren-extensions.scm
+++ miniKanren-extensions.scm
@@ -5,7 +5,7 @@
     (lambda ()
       (let ((n (walk n s)))
 	    (cond
-	      ((and (number? n) (>= 0 n)) (succeed s))
+	      ((and (integer? n) (>= 0 n)) (succeed s))
 	      ((var? n) ((nato-helper! n 0) s))
 	      (else (fail s)))))))
 
@@ -49,7 +49,7 @@
         ((cond
           ((and (number? n-1) (number? n) (eq? n-1 (- n 1))) succeed)
           ((and (number? n-1) (var? n)) (== n (+ n-1 1)))
-          ((and (var? n-1) (number? n)) (== n-1 (- n 1)))
+          ((and (var? n-1) (number? n)) (if (zero? n) fail (== n-1 (- n 1))))
           ((and (var? n-1) (var? n)) (succo-helper! n-1 n 0))
           (else fail)) s))))) ;; One of the number? checks must have failed. Type failure.
 
@@ -108,6 +108,17 @@
           ((and    (var? a)    (var? b)    (var? out)) (conj (nato a) (+o a b out)))
           (else fail)) s)))))
 
+(define (squareo x x2)
+  (lambda (s)
+    (lambda ()
+      (let ((x  (walk x s))
+            (x2 (walk x2 s)))
+        ((cond
+          ((and (number? x) (number? x2) (eq? (* x x) x2)) succeed)
+          ((and (number? x)    (var? x2)) (== x2 (* x x)))
+          ((and    (var? x) (number? x2)) (if (integer? (sqrt x2)) (== x (sqrt x2)) fail))
+          ((and    (var? x)    (var? x2)) (conj (nato x) (squareo x x2)))
+          (else fail)) s)))))
 
 #|
 (define (*o a b out)
@@ -179,12 +190,237 @@
           (else fail)) s)))))
 
 
-;; The power of relational programming!!!
-(defrel (goldbacho p a b)
-  (eveno p)
-  (+o a b p)
-  (primeo a)
-  (primeo b))
+(defrel (pairo x) (fresh (first rest) (== x `(,first . ,rest))))
+
+;; Goldbach conjecture
+;; "Every even number >= 4 can be written as the sum of two primes.
+
+;; even number -- n
+;; prime number -- p1
+;; prime number -- p2
+
+(defrel (goldbach-conjectureo n p1 p2)
+  (eveno n) ;; IDK / K
+  (+o p1 p2 n)
+  (primeo p1)
+  (primeo p2))
+
+
+(defrel (pythagorean-tripleo a b c)
+  (nato a)
+  (nato b)
+
+  (fresh (a2 b2 c2)
+    (squareo a a2)
+    (squareo b b2)
+    (+o a2 b2 c2)
+    
+    (squareo c c2))
+
+  (>o a 0)
+  (>o b 0))
+
+
+(defrel (weaveo l r out)
+  (conde
+    ((== l '()) (== r '()) (== out '()))
+    ((pairo l) (== r '()) (== out l))
+    ((== l '()) (pairo r) (== out r))
+    ((fresh (l-first l-rest r-first r-rest l-rec r-rec out-first out-rest)
+       (== l `(,l-first . ,l-rest))
+       (== r `(,r-first . ,r-rest))
+       (== out `(,out-first . ,out-rest))
+       
+       (conde
+         ((== out-first l-first) (== l-rec l-rest) (== r-rec r))
+         ((== out-first r-first) (== l-rec l) (== r-rec r-rest)))
+       
+       (weaveo l-rec r-rec out-rest)))))
+
+
+(defrel (three-partitiono nums partitions)
+  (fresh (sum) (three-partition-helpero nums partitions sum)))
+  
+(defrel (three-partition-helpero nums partitions sum)
+  (conde
+    ((== nums '()) (== partitions '()))
+    
+    ((fresh (a b ab c rest rest-partitions)
+      (== partitions `((,a ,b ,c) . ,rest-partitions))
+      (weaveo `(,a ,b ,c) rest nums)
+      (+o a b ab)
+      (+o ab c sum)
+      (three-partition-helpero rest rest-partitions sum)))))
+      
+
+(define partition-example
+  '(20 23 25 30 49 45 27 30 30 40 22 19))
+
+
+#|
+;; First draft -- line up recursive conditionals
+(defrel (picko n picked rest l)
+  (conde
+    ;; 
+    ((== n 0) (== picked '()) (== rest l))
+    
+    ((fresh (n-1 l-car l-cdr)
+      (== l `(,l-car . ,l-cdr))
+      (succo n-1 n)
+      (conde
+        ((fresh (rest-cdr)
+           (== rest `(,l-car . ,rest-cdr))
+           (picko n picked rest-cdr l-cdr)))
+
+        ((fresh (picked-cdr)
+           (== picked `(,l-car . ,picked-cdr))
+           (picko n-1 picked-cdr rest l-cdr))))))))
+|#     
+ 
+;; Second draft -- line up recursive conditionals
+(defrel (picko n picked rest l)
+  (conde
+    ;; 
+    ((== n 0) (== picked '()) (== rest l))
+    
+    ((fresh (n-1 l-car l-cdr)
+      (== l `(,l-car . ,l-cdr))
+      (succo n-1 n)
+      (fresh (x0 x1 x2)
+        (conde
+          ((fresh (rest-cdr)
+             (== rest `(,l-car . ,rest-cdr))
+             (== x0 n) (== x1 picked) (== x2 rest-cdr)))
+
+          ((fresh (picked-cdr)
+             (== picked `(,l-car . ,picked-cdr))
+             (== x0 n-1) (== x1 picked-cdr) (== x2 rest))))
+           
+        (picko x0 x1 x2 l-cdr))))))
+
+  
+(defrel (vertex-covero vertices edges cover)
+  (fresh (k rest-vertices)
+    (picko k cover rest-vertices vertices)
+    (every-edge-coveredo edges cover)))
+
+(define sample-vertices '(0 1 2 3 4 5 6 7))
+
+(define sample-edges
+  '((0 . 1)
+    (0 . 2)
+    (0 . 3)
+    (0 . 4)
+    (0 . 5)
+    (6 . 1)
+    (6 . 2)
+    (6 . 3)
+    (6 . 4)
+    (6 . 5)
+    (6 . 7)
+    (7 . 4)
+    (7 . 5)))
+
+(defrel (every-edge-coveredo edges cover)
+  (conde
+    ((== edges '()))
+    ((fresh (u v uv rest)
+      (== edges `((,u . ,v) . ,rest))
+      
+      (conde
+        ((== u uv))
+        ((== v uv)))
+
+      (containso cover uv)
+      (every-edge-coveredo rest cover)))))
+      
+(defrel (containso l x)
+  (fresh (first rest)
+    (== l `(,first . ,rest))
+    (conde
+      ((== x first))
+      ((containso rest x)))))
+
+;; TODO: force the mapping to be 1-to-1
+(defrel (subgraph-isomorphismo g-edges h-edges mapping)
+  (conde
+    (((== g-edges '()) (== mapping '()))
+    ((fresh (g-u g-v h-u h-v g-edges-cdr)
+      (== g-edges `((,g-u . ,g-v) . ,g-edges-cdr))
+      (lookupo g-u mapping h-u)
+      (lookupo g-v mapping h-v)
+      
+      (fresh (l r)
+        (conde
+          ((== h-u l) (== h-v r))
+          ((== h-u r) (== h-v l)))
+        
+        (containso h-edges l r))
+      
+      (subgraph-isomporphismo g-edges-cdr h-edges mapping))))))
+
+(defrel (rewriteo in out)
+  (fresh (ro a b c d e f x y z)
+    (== in `(conde ((,ro ,a ,b ,c)) ((,ro ,d ,e ,f))))
+    (== out
+      `(fresh (,x ,y ,z)
+        (conde
+          ((== ,x ,a) (== ,y ,b) (== ,z ,c))
+          ((== ,x ,d) (== ,y ,e) (== ,z ,f)))
+        (,ro ,x ,y ,z)))))
+
+
+
+(defrel (defrelo x)
+  (fresh (y z)
+    (== x `(defrel ,y . ,z))
+    (defrel-argso y)
+    (nonemptyo z)
+    (statementso z)))
+    
+(defrel (defrel-argso x)
+  (fresh (r first rest)
+    (== x `(,r ,first . ,rest))
+    (fresh-listo rest)))
+    
+(defrel (defrel-bodyo x)
+  (nonemptyo x)
+  (statementso x)
+  (fresh (first rest)
+    (== x `(,first . ,rest))
+    (statemento first)
+    (statementso rest)))
+
+(defrel (pairo x)
+  (fresh (l r) (== x `(,l . ,r))))
+
+(defrel (nonemptyo x)
+  (pairo x))
+  
+(defrel (statementso x)
+  (conde
+    ((== x '()))
+    ((fresh (first rest) (== x `(,first . ,rest))
+      (statemento first)
+      (statementso rest)))))
+      
+(defrel (statemento x)
+  (conde
+    ((fresh (r a) (== x `(,r ,a))))
+    ((fresh (r a b) (== x `(,r ,a ,b))))
+    ((fresh (r a b c) (== x `(,r ,a ,b ,c))))
+    ((fresh (fst snd)
+      (== x `(conde (,fst) (,snd)))
+      (statementso fst) (statementso snd)))
+    ((fresh (vars body) (== x `(fresh ,vars ,body)) (nonemptyo vars) (nonemptyo body) (fresh-listo vars) (statementso body)))))
+
+(defrel (fresh-listo l)
+  (conde
+    ((== l '()))
+    ((fresh (first rest) (== l `(,first . ,rest)) (fresh-listo rest))))) 
+  
+  
+
 
 (defrel (brett x)
   (== x 'foo))
@@ -222,10 +458,3 @@
   (differento b d)
   (differento c d)
   (differento d e))
-
-  
-  
-  
-
-
-
