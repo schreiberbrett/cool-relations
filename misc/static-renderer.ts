6f479023d594177f33b2e3c72c00f55133df18c2
chet <schreiberbrett@gmail.com>
2023-04-19 13:30:53 +0700

    Algorithm design, and interlinear work

---
+++ misc/static-renderer.ts
@@ -0,0 +1,79 @@
+/*
+
+This file is used as a part of the build process to convert the interlinear JSON translations into HTML.
+
+To run this file:
+$ deno run --allow-read --allow-write static-renderer.ts
+*/
+
+type InterlinearData = {
+    page: number,
+    text: {
+        km: string[],
+        en: (string | string[])[]
+    }[]
+}[];
+
+
+function toHTML(x: InterlinearData): string {
+    return `
+        ${x.map(passage =>
+            `<div class="full">
+                ${passage.text.map(segment =>
+                    `<div class="element">
+                        <div class="km">
+                            ${segment.km.map(word =>
+                                `<a href="https://en.wiktionary.org/wiki/${word}#Khmer">
+                                    ${word}
+                                </a>`
+                            ).join('\n')}
+                        </div>
+                        <div class="en">
+                            ${segment.en.map(word =>
+                                (word instanceof Array)
+                                    ? `<i>${word.join(' ')}</i>`
+                                    : word)
+                            .join(' ')}
+                        </div>
+                    </div>`).join('\n')}
+            </div>`).join('')}
+      
+        <style>
+            .full {
+                display: flex;
+                flex-wrap: wrap;
+                column-gap: 10px;
+            }
+        
+            .km {
+                font-size: xx-large;
+            }
+        
+            .en, .km {
+                text-align: center;
+            }
+        </style>
+    `;
+}
+
+
+let contents;
+try {
+    contents = await Deno.readTextFile('khmer-interlinear-plato.json');
+} catch (e) {
+    console.log(e.message);
+    Deno.exit(0);
+}
+
+const data = JSON.parse(contents) as InterlinearData;
+
+const html = toHTML(data);
+
+try {
+    await Deno.writeTextFile('plato.html', html);
+} catch (e) {
+    console.log(e.message);
+    Deno.exit(0);
+}
+
+
