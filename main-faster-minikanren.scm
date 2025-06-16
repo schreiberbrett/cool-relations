5124d249e52040d705ebf1fcbe69398edec66302
Brett Schreiber <schreiberbrett@gmail.com>
2025-06-16 00:50:09 -0400

    1-in-3SAT

---
+++ main-faster-minikanren.scm
@@ -0,0 +1,5 @@
+(load "../faster-minikanren/mk-vicare.scm")
+(load "../faster-minikanren/mk.scm")
+(load "../faster-minikanren/numbers.scm")
+(load "cool-relations.html")
+(load "cool-relations-faster-minikanren.html")
